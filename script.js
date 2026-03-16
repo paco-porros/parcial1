22eed0a8d1e0ec37505242ad4c9c883e085b824e
Victor López <lopez.rios2105@gmail.com>
2026-03-16 10:27:47 -0500

    toma de variables del html al js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('button').addEventListener('click',CalcularRiesgo);
+function CalcularRiesgo(){
+    const edad = parseFloat(document.getElementById('edad').value);
+    const presion = parseFloat(document.getElementById('presion').value);
+    const colesterol = parseFloat(document.getElementById('colesterol').value);
+    const fumador = parseFloat(document.getElementById('fumador').value);
+}
